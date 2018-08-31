859a62da477034c4f64446f47f26656746500347
ddulaney <dulaneyredbarn77@yahoo.com>
2018-08-31 15:54:30 -0700

    finished javascript functions for modal, filter and random generator.  added styling to seach, container and modal.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 // //random generator API////You can use AJAX to call the Random User Generator API and will receive a randomly generated user in return. If you are using jQuery, you can use the $.ajax() function in the code snippet below to get started.
 const container = document.querySelector('.container');
 const modalSection = document.querySelector('.modalSection');
+let employees;
 
 $.ajax({
   url: 'https://randomuser.me/api/?results=12&nat=us',
   dataType: 'json',
   success: function(data) {
-    const employees = data.results;
+    employees = data.results;
     directory(employees);
   }
 });
  
 function directory(employees) {
-  
   for (const employee in employees) {
-    let image = employees[employee].picture.large;
-    let firstName = employees[employee].name.first;
-    let lastName = employees[employee].name.last;
-    let fullName = `${firstName} ${lastName}`;    
-    let email = employees[employee].email;
-    let city = employees[employee].location.city;
-    
+  let image = employees[employee].picture.large;
+  let firstName = employees[employee].name.first;
+  let lastName = employees[employee].name.last;
+  let fullName = `${firstName} ${lastName}`;    
+  let email = employees[employee].email;
+  let city = employees[employee].location.city;
+   
     container.innerHTML += `
     <div class="box">    
       <div class="image-box">
@@ -35,70 +35,75 @@ function directory(employees) {
           </li>
         </div>
     </div>
-    `
+    `;
   }
-  }
 
-/////////////////////////////modal/////////////////////////////////
-//Get modal element
-var modal = document.getElementById('simpleModal');
-// Get open modal button
-var modalBtn = document.getElementById('modalBtn');
-// Get close button
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
 
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
-// Listen for outside click
-window.addEventListener('click', outsideClick);
+// Get input element
+let filterInput = document.getElementById('search');
+// Add event listener
+filterInput.addEventListener('keyup', filterNames);
 
-// Function to open modal
-function openModal(){
-  modal.style.display = 'block';
+function filterNames(){
+  // Get value of input
+  const employeeBox = document.querySelectorAll('.box');
+  let filterValue = filterInput.value.toUpperCase();
+  let employeeNames = document.querySelectorAll('.name');
+
+  // // Get lis from ul
+  employeeNames.forEach((name, index) => {
+   if(name.innerHTML.toUpperCase().indexOf(filterValue) > -1) {
+     employeeBox[index].style.display = "grid";
+   } else {
+    employeeBox[index].style.display = "none";
+   }
+  });
+
+  // // Loop through collection-item lis
+  // for(let i = 0;i < li.length;i++){
+  //   let a = li[i].getElementsByTagName('a')[0];
+  //   // If matched
+  //   if(a.innerHTML.toUpperCase().indexOf(filterValue) > -1){
+  //     li[i].style.display = '';
+  //   } else {
+  //     li[i].style.display = 'none';
+  //   }
+  // }
+
 }
 
-// Function to close modal
-function closeModal(){
-  modal.style.display = 'none';
-}
 
-// Function to close modal if outside click
-function outsideClick(e){
-  if(e.target == modal){
-    modal.style.display = 'none';
+  const employeeBox = document.querySelectorAll('.box');
+  employeeBox.forEach((box, index) => {
+    box.addEventListener('click',  function(){
+      modalSection.style.display = "block";
+      buildModal(employees[index], index);
+    });
+  });
+ 
+  
+
+
   }
-}
 
-/////////////display in modeal////////////////
-// Image
-// Name
-// Email
-// Cell Number
-// Detailed Address, including street name and number, city, state and post code.
-// Birthdate
 
-function directory(employees) {
-
-for (const employee in employees) {
-
-let image = employees[employee].picture.large;
-let firstName = employees[employee].name.first;
-let lastName = employees[employee].name.last;
+function buildModal(employee, index) {
+let image = employee.picture.large;
+let firstName = employee.name.first;
+let lastName = employee.name.last;
 let fullName = `${firstName} ${lastName}`;    
-let email = employees[employee].email;
-let city = employees[employee].location.city;
+let email = employee.email;
+let city = employee.location.city;
 
-let streetAddress = employees[employee].location.street;
-let state = employees[employee].location.state;
-let postCode = employees[employee].location.postcode;
-let birthday = employees[employee].dob;
-let cell = employees[employee].cell;
+let streetAddress = employee.location.street;
+let state = employee.location.state;
+let postCode = employee.location.postcode;
+let dateOfBirth = new Date(Date.parse(employee.dob.date)).toLocaleDateString();
+let cell = employee.cell;
 let cityStateZip =`${city} , ${state}   ${postCode}`;
 
-modalSection.innerHTML += `
-<div id="simpleModal" class="modal" onclick="this.parentNode.style.display='none'">
+modalSection.innerHTML = `
+<div id="simpleModal" class="modal">
  <div class="modal-content">
   <div class="modal-header">
       <span class="closeBtn">&times;</span>
@@ -116,7 +121,7 @@ modalSection.innerHTML += `
          <p class="cellPhone">${cell}</p>
        <p class="street-address">${streetAddress}</p>
           <p class="city">${cityStateZip}</p>         
-        <p class="b-date">${birthday}</p>
+        <span id="date">${dateOfBirth}</span>
    </div>
  <div class="back">
     <p>&#60</p>
@@ -127,7 +132,56 @@ modalSection.innerHTML += `
  </div>    
  </div>
  </section> 
-`
-}
-}
+`;
+
  
+// /////////////////////////////modal/////////////////////////////////
+//Get modal element
+const modal = document.getElementById('simpleModal');
+// Get open modal button
+
+// Get close button
+const closeBtn = document.getElementsByClassName('closeBtn')[0];
+const forward = document.querySelector('.forward');
+const back = document.querySelector('.back');
+const arrayLength = (employees.length -1);
+// // Listen for open click
+
+
+forward.addEventListener('click', function(){
+
+  if(index < arrayLength){
+    buildModal(employees[index + 1], index + 1);
+  } else {
+    buildModal(employees[0], 0);
+  }
+
+});
+
+back.addEventListener('click', function(){
+  if(index > 0) {
+  buildModal(employees[index - 1], index - 1);
+  } else {
+    buildModal(employees[arrayLength], arrayLength);
+  }
+});
+
+// // Listen for close click
+closeBtn.addEventListener('click', closeModal);
+//
+ // Listen for outside click
+window.addEventListener('click', outsideClick);
+
+
+// Function to close modal
+function closeModal(){
+  modalSection.style.display = 'none';
+}
+
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target === modal){
+    modal.style.display = 'none';
+  }
+}
+}
